d858524badd5e196a113f78ea3d3eb61d64ac655
RawanD201 <ra.one77@outlook.com>
2022-09-06 15:05:48 +0300

    fix controller

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserLogsController;
-use App\Http\Controllers\auth\ChangePasswordController;
+use App\Http\Controllers\Auth\ChangePasswordController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\IndexController;
 
